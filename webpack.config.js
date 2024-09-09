11f7ad399769685117f5782e7943136090ca4f44
Diego Gutierrez <diagutierrezro@unal.edu.co>
2024-09-09 12:06:33 -0500

    fix: Ajuste para despliegue

--- webpack.config.js
+++ webpack.config.js
@@ -19,10 +19,31 @@ module.exports = (webpackConfigEnv, argv) => {
         inject: false,
         template: "src/index.ejs",
         templateParameters: {
+          isProd: webpackConfigEnv && webpackConfigEnv.isProd,
+          isDev: webpackConfigEnv && webpackConfigEnv.isDev,
           isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
           orgName,
         },
+        favicon: "./src/favicon.ico",
       }),
+      new DefinePlugin({
+        isProd: webpackConfigEnv && webpackConfigEnv.isProd,
+        isDev: webpackConfigEnv && webpackConfigEnv.isDev,
+        isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: 'src/assets/i18n', to: 'assets/i18n' }
+        ]
+      })
     ],
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'dist'),
+      },
+      compress: true,
+      port: 4200,
+      historyApiFallback: true,
+    }
   });
 };
