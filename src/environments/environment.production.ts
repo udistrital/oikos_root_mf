56a44ccb27f0a708d62eaaaf0cd4c6e47ab622fa
Diego Gutierrez <diagutierrezro@unal.edu.co>
2024-09-09 12:00:14 -0500

    fix: Ajuste para despliegue a pruebas

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,29 @@
+export const environment = {
+    production: true,
+    entorno: "test",
+    autenticacion: true,
+    notificaciones: false,
+    menuApps: false,
+    appname: "oikos",
+    appMenu: "OIKOS_MF",
+    TOKEN: {
+      AUTORIZATION_URL:
+        "https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize",
+      CLIENTE_ID: "e36v1MPQk2jbz9KM4SmKhk8Cyw0a",
+      RESPONSE_TYPE: "id_token token",
+      SCOPE: "openid email role documento",
+      REDIRECT_URL: "http://localhost:4200/",
+      SIGN_OUT_URL:
+        "https://autenticacion.portaloas.udistrital.edu.co/oidc/logout",
+      SIGN_OUT_REDIRECT_URL: "http://localhost:4200/",
+      AUTENTICACION_MID:
+        "https://autenticacion.portaloas.udistrital.edu.co/apioas/autenticacion_mid/v1/token/userRol",
+    },
+    parcels: {
+      "@udistrital/core-mf": "https://pruebascoreclientes.portaloas.udistrital.edu.co/main.js",
+      "@udistrital/root-config": "https://pruebasoikosmicroclientes.portaloas.udistrital.edu.co/udistrital-root-config.js",
+      "@udistrital/mapeo": "https://pruebasoikosmapeodependencias.portaloas.udistrital.edu.co/main.js",
+      "@udistrital/registro-gestion": "https://pruebasoikosgestiondependencias.portaloas.udistrital.edu.co/main.js"
+    }
+  };
+  
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,13 +7,22 @@ export const environment = {
   appname: "oikos",
   appMenu: "OIKOS_MF",
   TOKEN: {
-    AUTORIZATION_URL: "https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize",
+    AUTORIZATION_URL:
+      "https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize",
     CLIENTE_ID: "e36v1MPQk2jbz9KM4SmKhk8Cyw0a",
     RESPONSE_TYPE: "id_token token",
     SCOPE: "openid email role documento",
     REDIRECT_URL: "http://localhost:4200/",
-    SIGN_OUT_URL: "https://autenticacion.portaloas.udistrital.edu.co/oidc/logout",
+    SIGN_OUT_URL:
+      "https://autenticacion.portaloas.udistrital.edu.co/oidc/logout",
     SIGN_OUT_REDIRECT_URL: "http://localhost:4200/",
-    AUTENTICACION_MID: "https://autenticacion.portaloas.udistrital.edu.co/apioas/autenticacion_mid/v1/token/userRol",
-  }  
+    AUTENTICACION_MID:
+      "https://autenticacion.portaloas.udistrital.edu.co/apioas/autenticacion_mid/v1/token/userRol",
+  },
+  parcels: {
+    "@udistrital/root-config": "//localhost:4200/udistrital-root-config.js",
+    "@udistrital/core-mf": "//localhost:4201/main.js",
+    "@udistrital/registro-gestion": "//localhost:4202/main.js",
+    "@udistrital/mapeo": "//localhost:4203/main.js"
+  }
 };
