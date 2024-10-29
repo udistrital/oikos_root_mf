3a1237c8554102e775dc6090a3b1cae6fe2ef654
Diego Gutierrez <diagutierrezro@unal.edu.co>
2024-10-29 14:08:22 -0500

    fix: Ajuste en drone

--- src/udistrital-root-config.ts
+++ src/udistrital-root-config.ts
@@ -11,8 +11,6 @@ let environment;
 
 declare var isProd: boolean | undefined;
 declare var isDev: boolean | undefined;
-console.log("IS PROD ", isProd);
-console.log("IS DEV ", isDev);
 if (isProd) {
   environment = require("./environments/environment.production");
 } else if (isDev) {
@@ -25,7 +23,6 @@ const data = {
   props: environment,
   loaders: {},
 };
-console.log("Data ", data);
 
 const routes = constructRoutes(microfrontendLayout, data);
 const applications = constructApplications({
@@ -34,7 +31,6 @@ const applications = constructApplications({
     return System.import(name);
   },
 });
-console.log(applications);
 const layoutEngine = constructLayoutEngine({ routes, applications });
 
 applications.forEach(registerApplication);
